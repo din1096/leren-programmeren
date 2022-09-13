5d3bab7dd42b0f20704a2716503f390b53116716
dinho <99069865@mydavinci.nl>
2022-09-13 09:46:34 +0200

    alles owo

---
+++ software_developer/leerroutes/LerenProgrammeren/module1/py.py
@@ -0,0 +1,11 @@
+print('Hello wereld van')
+print("""
+    pppp    y     y   ttttttt   h   h    oooo   nnnn       
+    p   p    y   y       t      h   h   o    o  n   n
+    p   p     y y        t      h   h   o    o  n   n
+    pppp       y         t      hhhhh   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h    oooo   n   n
+""")
+#comments dinho hooi 99069865

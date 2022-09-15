1354f3b48b9b43ed4fc2f507d89a6a70f68ee39f
din1096 <113169715+din1096@users.noreply.github.com>
2022-09-15 15:08:23 +0200

    Add files via upload

---
+++ software_developer/leerroutes/LerenProgrammeren/module1/speelhal.py
@@ -0,0 +1,7 @@
+
+toegangsticket = int(input('hoeveel mensen gaan mee'))
+toegangsticket = (toegangsticket * 7.45 )
+gameseat = int(input('hoeveel lang ga je er in '))
+gameseat = (gameseat * 0.37 )
+
+print (toegangsticket + gameseat)

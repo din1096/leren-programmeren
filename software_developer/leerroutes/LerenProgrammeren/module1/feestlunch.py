66bfa8f9a3699c8cda16c0258e8946a827106915
din1096 <113169715+din1096@users.noreply.github.com>
2022-09-15 15:47:25 +0200

    Add files via upload

---
+++ software_developer/leerroutes/LerenProgrammeren/module1/feestlunch.py
@@ -0,0 +1,15 @@
+
+
+aantal_croissantjes = int(input('hoeveel croissantjes wil je')) 
+croissantjes_prijs = (aantal_croissantjes * 0.39)
+
+aantal_stokbrood = int(input('hoeveel stokbrood wil je')) 
+stokbrood_prijs = (aantal_stokbrood * 2.78 )
+
+aantal_kortingsbonnen = int(input('hoeveel kortingsbonnen heb je'))
+kortingsbonnen_prijs = (aantal_kortingsbonnen * 0.50 )
+
+print(f'de feestlunch kost bij je bakker {croissantjes_prijs + stokbrood_prijs - kortingsbonnen_prijs} voor {aantal_croissantjes} croissantjes en de {aantal_stokbrood} stokbroden als de {aantal_kortingsbonnen} kortingsbonnen nog geldig zijn!')
+
+
+

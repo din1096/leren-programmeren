046bfa4c6c85a686e59606547da56118da58acc5
din1096 <113169715+din1096@users.noreply.github.com>
2022-09-15 13:35:59 +0200

    Add files via upload

--- speelhal.py
+++ speelhal.py
@@ -1,6 +1,8 @@
-import math
+toegangsticket = 7.45 
+personen = 4
+vipgameseat = 0.37 
+tijd = 45
 
-toegangsticket = 7.45 * 4
-gameseat = 0.37 * 45
+print('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro')
 
-print(f"dan moet je in totaal {toegangsticket + gameseat}")
+print(f"dan moet je in totaal voor de toeganstickest {toegangsticket * personen} en je moet voor je vipgameseat {vipgameseat * tijd} betalen") 

e2718d8c2a92cc25d7cda62ac8261b13d00c59a2
din1096 <113169715+din1096@users.noreply.github.com>
2022-09-15 11:43:39 +0200

    Add files via upload

--- software_developer/leerroutes/LerenProgrammeren/module1/speelhal.py
+++ software_developer/leerroutes/LerenProgrammeren/module1/speelhal.py
@@ -1,6 +1,8 @@
-import math
+import math 
 
-toegangsticket = 7.45 * 4
-gameseat = 0.37 * 45
+toegangsticket = 7.45 
+personen = 4
+vipgameseat = 0.37 
+tijd = 45
 
-print(f"dan moet je in totaal {toegangsticket + gameseat}")
+print(f"dan moet je in totaal voor de toeganstickest {toegangsticket * personen} en je moet voor je vipgameseat {vipgameseat * tijd}") 

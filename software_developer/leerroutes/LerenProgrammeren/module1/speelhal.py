51ab75b2463d01af7f9e2ac274b029cd414bb28f
din1096 <113169715+din1096@users.noreply.github.com>
2022-09-15 15:08:06 +0200

    Delete speelhal.py

--- software_developer/leerroutes/LerenProgrammeren/module1/speelhal.py
+++
@@ -1,6 +0,0 @@
-
-toegangsticket = 7.45 
-personen = 4
-vipgameseat = 0.37 
-tijd = 45
-print(f"dan moet je in totaal voor de toeganstickest {toegangsticket * personen} en je moet voor je vipgameseat {vipgameseat * tijd} betalen") 

df94644b422bc63bef6b4f99860c9ecb103c0772
din1096 <113169715+din1096@users.noreply.github.com>
2022-09-15 13:32:01 +0200

    Add files via upload

--- software_developer/leerroutes/LerenProgrammeren/module1/feestlunch.py
+++ software_developer/leerroutes/LerenProgrammeren/module1/feestlunch.py
@@ -1,12 +1,5 @@
-from cgi import print_arguments
-import math
-
 croissantjes =  0.39 * 17 
 stokbroden = 2.78 * 2
 kortingsbonnen = 0.50 * 3
 
-print(f"dan kost het in totaal {croissantjes + stokbroden - kortingsbonnen}")
-
-
-
-
+print(f" het kost in totaal {croissantjes + stokbroden - kortingsbonnen}")

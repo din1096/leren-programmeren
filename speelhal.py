2966b6b43921cff230f33e6cd028d45f7e20c10a
din1096 <113169715+din1096@users.noreply.github.com>
2022-09-16 08:55:24 +0200

    Add files via upload

---
+++ speelhal.py
@@ -0,0 +1,7 @@
+
+aantal_toegangsticket = int(input('hoeveel tickets wil je'))
+toegangsticket_prijs  = (aantal_toegangsticket * 7.45 )
+aantal_gameseat = int(input('hoeveel lang ga je er in '))
+gameseat_prijs = (aantal_gameseat * 0.37 )
+
+print(f'dit geweldige dagje-uit met {aantal_toegangsticket} mensen in de speelhal met {aantal_gameseat} vr kost je maar 44.44 euro')

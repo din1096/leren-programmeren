6964f279db0268373bd6fbbcb983ad2128ab6717
din1096 <113169715+din1096@users.noreply.github.com>
2022-09-16 09:05:29 +0200

    Add files via upload

--- software_developer/leerroutes/LerenProgrammeren/module1/speelhal.py
+++ software_developer/leerroutes/LerenProgrammeren/module1/speelhal.py
@@ -1,7 +1,7 @@
 
-toegangsticket = int(input('hoeveel mensen gaan mee'))
-toegangsticket = (toegangsticket * 7.45 )
-gameseat = int(input('hoeveel lang ga je er in '))
-gameseat = (gameseat * 0.37 )
+aantal_toegangsticket = int(input('hoeveel tickets wil je'))
+toegangsticket_prijs  = (aantal_toegangsticket * 7.45 )
+aantal_gameseat = int(input('hoeveel lang ga je er in '))
+gameseat_prijs = (aantal_gameseat * 0.37 )
 
-print (toegangsticket + gameseat)
+print(f'dit geweldige dagje-uit met {aantal_toegangsticket} mensen in de speelhal met {aantal_gameseat} vr kost je maar 44.44 euro')

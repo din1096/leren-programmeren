7f8271a312a14baaffa3bd85ffc78b22781b043d
dinho <99069865@mydavinci.nl>
2022-09-16 10:08:05 +0200

    speelhal_aangepast

--- software_developer/leerroutes/LerenProgrammeren/module1/feestlunch.py
+++ software_developer/leerroutes/LerenProgrammeren/module1/feestlunch.py
@@ -5,7 +5,8 @@ croissantjes_prijs = (aantal_croissantjes * 0.39)
 
 aantal_stokbrood = int(input('hoeveel stokbrood wil je')) 
 stokbrood_prijs = (aantal_stokbrood * 2.78 )
-
+print(type(stokbrood_prijs))
+print('hallo')
 aantal_kortingsbonnen = int(input('hoeveel kortingsbonnen heb je'))
 kortingsbonnen_prijs = (aantal_kortingsbonnen * 0.50 )
 

fd7dcea99d83b63f1f74e3da9bb50e4ba6baaf97
dinho <99069865@mydavinci.nl>
2022-09-22 12:10:14 +0200

    'challenge'

--- software_developer/leerroutes/LerenProgrammeren/challenges/smartphone.py
+++ software_developer/leerroutes/LerenProgrammeren/challenges/smartphone.py
@@ -1,17 +1,17 @@
 prijs_iphone = int(input('hoeveel kost een iphone'))
 prijs_samsung = int(input('hoeveel kost een samsung'))
 
+verschil = prijs_samsung - prijs_iphone
 
-if prijs_iphone < prijs_samsung :
-    print(f'De samsung is het duurst,de telefoon kost{prijs_samsung} euro')
-    print(f'De iphone is het goedkoopst, de telefoon{prijs_iphone} euro')
-    print(f'Het advies is dus de iphone te kopen.Deze is  namelijk{prijs_samsung - prijs_iphone} euro goedkoper dan de samsung galaxy s22')
+if verschil > 50 :
+    print(f'de samsung is het duurst de telefoon kost {prijs_samsung} euro')
+    print(f'de iphone is het goedkoopst de telefoon kost {prijs_iphone} euro')
+    print(f'het advies is dus de iphone te kopen. deze is namelijk {prijs_samsung - prijs_iphone} goedkoper dan de samsung')
 
-elif prijs_iphone > prijs_samsung :
-    print(f'De iphone is het duurst,De telefoon kost {prijs_iphone} euro')
-    print(f'De samsung is het goedkoopst,de telefoon kost {prijs_samsung} euro')
-    print(f'Het advies is dus de samsung te kopen.deze is namelijk {prijs_iphone - prijs_samsung} euro goedkoper dan de iphone 13')
+elif verschil < 50 :
+    print(f'de iphone is het duurst de telefoon kost{prijs_iphone} euro')
+    print(f'de samsung is het goedkoopst de telefoon kost{prijs_samsung} euro ')
+    print(f'het advies is dus de samsung te kopen deze is namelijk {prijs_iphone - prijs_samsung} euro goedkoper dan de iphone')
     
-elif prijs_iphone / prijs_samsung :
-    print(f'niks is het duurst ze kosten allenbij het zelfde')
-    print(f'het advies is dus de iphone te kopen deze is namelijk {prijs_iphone - prijs_samsung} euro goedkoper dan de samsung')
+elif prijs_iphone == prijs_iphone :
+    print(f'het advies is dus om de iphone te kopen  deze is namelijk {prijs_samsung - prijs_iphone} goedkoper dan de samsung')    

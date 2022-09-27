1be8e541739ffc8c0d4c033cd9c14aff11ef809d
dinho <99069865@mydavinci.nl>
2022-09-27 08:59:33 +0200

    'update'

--- software_developer/leerroutes/LerenProgrammeren/module2/speelhal.py
+++ software_developer/leerroutes/LerenProgrammeren/module2/speelhal.py
@@ -1,8 +1,8 @@
 
-aantal_toegangsticket = float(input('hoeveel tickets wil je'))
-toegangsticket_prijs  = (aantal_toegangsticket * 745/100 )
-tijd_gameseat = float(input('hoelang ga je er in '))
-gameseat_prijs = (tijd_gameseat * 37/100 )
+aantal_toegangsticket = int(input('hoeveel tickets wil je'))
+toegangsticket_prijs  = (aantal_toegangsticket * 745 )
+tijd_gameseat = int(input('hoelang ga je er in '))
+gameseat_prijs = (tijd_gameseat * 37 )
+totaal_prijs = (toegangsticket_prijs + gameseat_prijs) / 100
 
-
-print(f'dit geweldige dagje-uit met {aantal_toegangsticket} mensen in de speelhal met {tijd_gameseat} minuten vr kost je maar {toegangsticket_prijs + gameseat_prijs} euro')
+print(f'dit geweldige dagje-uit met {aantal_toegangsticket} mensen in de speelhal met {tijd_gameseat} minuten vr kost je maar {totaal_prijs} euro')

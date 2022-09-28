8e9a7e4a10f7a6b827cbd302c55df46f4a727796
dinho <99069865@mydavinci.nl>
2022-09-28 13:53:16 +0200

    'else'

--- software_developer/leerroutes/LerenProgrammeren/module3/meten is weten.py
+++ software_developer/leerroutes/LerenProgrammeren/module3/meten is weten.py
@@ -1,13 +1,20 @@
 a = int(input('voer een getal in'))
 b = int(input('voer een getal in'))
 
-if a > b :
-   max = a 
-   print(f'a is het grootste getal {max}')
 
-elif a < b :
+if a > b:
+   max = a
+   print(f'a is het grootste getal is {max}')
+
+
+elif a < b:
    min = a
-   print(f'a is het kleinste getal {min}')
-   
-elif a == b :
+   print(f'a is het kleinste getal is {min}')
+
+
+else:
    print('a en b zijn even groot')
+   
+print(f'het minimun is {min}')
+print(f'het maximum is {max}')
+  

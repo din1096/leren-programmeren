ce75363d2f3eebb31a111ba40de7d81cdb011aef
dinho <99069865@mydavinci.nl>
2022-09-28 10:22:00 +0200

    'input'

--- software_developer/leerroutes/LerenProgrammeren/module2/pizzaCalculator.py
+++ software_developer/leerroutes/LerenProgrammeren/module2/pizzaCalculator.py
@@ -4,11 +4,16 @@ pizza_small = int(input('hoeveel kleine pizza wil je'))
 pizza_medium = int(input('hoeveel medium pizza wil je'))
 pizza_large = int(input('hoeveel grote pizza wil je'))
 
-pizza_prijs_s = (pizza_small * 7.77)
-pizza_prijs_m = (pizza_medium * 10.80)
-pizza_prijs_l = (pizza_large * 12.99)
+pizza_prijs_s = pizza_small * 7.77
+pizza_prijs_m = pizza_medium * 10.80
+pizza_prijs_l = pizza_large * 12.99
 totaal_prijs = pizza_prijs_s + pizza_prijs_m + pizza_prijs_l
 pizza_totaal = pizza_small + pizza_medium + pizza_large
+
+pizza_prijs_s = round (pizza_prijs_s, 2);
+pizza_prijs_m = round (pizza_prijs_m,2);
+pizza_prijs_l = round(pizza_prijs_l,2);
+totaal_prijs = round(totaal_prijs,2);
 
 print('-----------------------------------------------------------------------')
 print(f'het kost voor de {pizza_small} kleine pizza {pizza_prijs_s} euro')
@@ -16,7 +21,3 @@ print(f'het kost voor de {pizza_medium} medium pizza {pizza_prijs_m} euro')
 print(f'het kost voor de {pizza_large} grote pizza {pizza_prijs_l} euro')
 print(f'het kost voor alle {pizza_totaal} pizza {totaal_prijs} euro ')
 print('------------------------------------------------------------------------')
-pizza_prijs_s = round (pizza_prijs_s, 2);
-pizza_prijs_m = round (pizza_prijs_m,2);
-pizza_prijs_l = round(pizza_prijs_l,2);
-totaal_prijs = round(totaal_prijs,2);
---
+++ software_developer/leerroutes/LerenProgrammeren/module3/meten is weten.py
@@ -0,0 +1,7 @@
+a = int(input('voer een getal in'))
+b = int(input('voer een getal in'))
+
+max = a
+
+if a > b :
+   print(f'a is het grootste getal {max}')

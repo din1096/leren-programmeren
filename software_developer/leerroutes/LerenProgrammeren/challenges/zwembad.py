a288438195d1e89d102eed330729c935cb5ffea0
dinho <99069865@mydavinci.nl>
2022-10-13 09:48:36 +0200

    'update'

--- software_developer/leerroutes/LerenProgrammeren/challenges/zwembad.py
+++ software_developer/leerroutes/LerenProgrammeren/challenges/zwembad.py
@@ -1,6 +1,14 @@
-meter_lang = int(8)
-meter_breed = int (3)
-meter_diep = int(1.5)
-grond = meter_breed * meter_lang * meter_diep
+meter_lang = 8
+meter_breed = 3
+meter_diep = 1.5
+grond = meter_lang * meter_breed * meter_diep
+uitgraven_prijs = 25 * grond
+afvoeren_prijs = 32.50 * grond
+totaal_prijs = uitgraven_prijs + afvoeren_prijs
 
 print(f'in totaal moet {grond} grond worden afgevoerd')
+
+print('offerte voor de een zwembad van 8 bij 3 bij 1.5 meter')
+print(uitgraven_prijs)
+print(afvoeren_prijs)
+print(totaal_prijs)

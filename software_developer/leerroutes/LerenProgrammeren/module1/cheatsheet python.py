d267de8fce78387e9f132806363777099e54125b
dinho <99069865@mydavinci.nl>
2022-11-02 10:06:22 +0100

    'except'

--- software_developer/leerroutes/LerenProgrammeren/module1/cheatsheet python.py
+++ software_developer/leerroutes/LerenProgrammeren/module1/cheatsheet python.py
@@ -10,14 +10,9 @@
  #?/n"
 # if
 
-#variable = round {variable, 2};
-
-print(f'in totaal moet {grond} grond worden afgevoerd')
-print(f'offerte voor de een zwembad van {meter_lang} bij {meter_breed} bij {meter_diep} meter')
-print(f'het kost {uitgraven_prijs} euro')
-print (f'het kost {afvoeren_prijs} euro')
-print(f'het kost {voorrijk_prijs} euro')
-print(f'het kost in totaal {totaal_prijs} euro')
+#variable = round {variable, 2};   
+#datatypes
+#iets wat je een variable geeft str int float
 #boolean
 
 # build in funtie
@@ -29,7 +24,7 @@ print(f'het kost in totaal {totaal_prijs} euro')
 #type
 #return 
 # or
-meter_m2 = lengte * diepte * 2
+
 #comparison operators
 #Less than ( < )
 #Less than or equal to ( <= )
--- software_developer/leerroutes/LerenProgrammeren/module2/pizzaCalculator.py
+++ software_developer/leerroutes/LerenProgrammeren/module2/pizzaCalculator.py
@@ -1,37 +1,34 @@
 # dinho hooi pizza calculator
-from distutils.log import error
 
 try:
     pizza_small = int(input('hoeveel kleine pizza wil je'))
-except:
+except ValueError:
     print('vul een heel getal')
+
 
 try:
     pizza_medium = int(input('hoeveel medium pizza wil je'))
-except:
-    print('vul een heel getal in')
-try:
-    pizza_large = int(input('hoeveel grote pizza wil je'))
-except:
+except ValueError:
     print('vul een heel getal in')
 
-prijs_s = 7,77
-prijs_m = 10,80
-prijs_l = 12,99
+try:
+    pizza_large = int(input('hoeveel grote pizza wil je'))
+except ValueError:
+    print('vul een heel getal in')
+
+prijs_s = 7.77
+prijs_m = 10.80
+prijs_l = 12.99
+
 pizza_prijs_s = pizza_small * prijs_s
 pizza_prijs_m = pizza_medium * prijs_m
 pizza_prijs_l = pizza_large * prijs_l
 totaal_prijs = pizza_prijs_s + pizza_prijs_m + pizza_prijs_l
 pizza_totaal = pizza_small + pizza_medium + pizza_large
 
-pizza_prijs_s = round (pizza_prijs_s, 2);
-pizza_prijs_m = round (pizza_prijs_m,2);
-pizza_prijs_l = round(pizza_prijs_l,2);
-totaal_prijs = round(totaal_prijs,2);
-
 print('-----------------------------------------------------------------------')
-print(f'het kost voor de {pizza_small} kleine pizza {pizza_prijs_s} euro')
-print(f'het kost voor de {pizza_medium} medium pizza {pizza_prijs_m} euro')
-print(f'het kost voor de {pizza_large} grote pizza {pizza_prijs_l} euro')
-print(f'het kost voor alle {pizza_totaal} pizza {totaal_prijs} euro ')
+print(f'het kost voor de {pizza_small} kleine pizza {pizza_prijs_s:.2f} euro')
+print(f'het kost voor de {pizza_medium} medium pizza {pizza_prijs_m:.2f} euro')
+print(f'het kost voor de {pizza_large} grote pizza {pizza_prijs_l:.2f} euro')
+print(f'het kost voor alle {pizza_totaal} pizza {totaal_prijs:.2f} euro ')
 print('------------------------------------------------------------------------')

c26274439cac189e489714b75792bd038610f538
dinho <99069865@mydavinci.nl>
2023-01-31 14:41:04 +0100

    'update'

---
+++ software_developer/leerroutes/LerenProgrammeren/module4/boodschaplijstje.py
@@ -0,0 +1,11 @@
+boodschappen_lijst = {}
+lijst = True
+while lijst:
+    boodschappen = input('wat wil je als boodschappen').lower()
+    hoeveel = input('hoeveel wil je ervan')
+    toevoegen = input('wil je nog wat toevoegen')
+    boodschappen_lijst.update({boodschappen : hoeveel})
+    if toevoegen == 'nee':
+        lijst == False
+    for x in range(len(boodschappen_lijst)):
+        print(x[boodschappen_lijst])

717248fcace99e4a6f55deeb04fa8ed61a9a2fbe
dinho <99069865@mydavinci.nl>
2023-03-09 16:04:38 +0100

    'update'

---
+++ software_developer/leerroutes/LerenProgrammeren/fruit/opdracht03.py
@@ -0,0 +1,3 @@
+from fruitmand import fruitmand
+for x in range(1,8):
+    print(fruitmand[0]['name'])
---
+++ software_developer/leerroutes/LerenProgrammeren/fruit/opdracht04.py
@@ -0,0 +1,7 @@
+from fruitmand import fruitmand 
+import random
+
+aantal = int(input('hoeveel fruit wil je'))
+
+for x in aantal:
+    print(random.choice(fruitmand))

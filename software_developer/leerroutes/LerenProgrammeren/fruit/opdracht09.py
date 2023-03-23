9b7f33dae0299666c87bf60b5889ecbe2f9de105
dinho <99069865@mydavinci.nl>
2023-03-23 14:20:45 +0100

    'update'

--- software_developer/leerroutes/LerenProgrammeren/fruit/opdracht09.py
+++
@@ -1,7 +0,0 @@
-from fruitmand import fruitmand
-for i in range(len(fruitmand)):
-    if fruitmand[i]['name'] == 'druif':
-        del fruitmand[i]
-        break
-for fruit in fruitmand:
-    print(fruit['color'])
--- software_developer/leerroutes/LerenProgrammeren/fruit/opdracht10.py
+++
@@ -1,6 +0,0 @@
-from fruitmand import fruitmand
-from operator import itemgetter
-fruitmand = sorted(fruitmand , key = itemgetter("weight"),reverse=True)
-for fruit in fruitmand:
-        print(fruit["weight"])
-    

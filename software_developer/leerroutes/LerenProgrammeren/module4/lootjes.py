77261953b0cfb7c237f51987cdf97ea3dc5f924f
dinho <99069865@mydavinci.nl>
2023-03-31 10:25:57 +0200

    'update'

---
+++ software_developer/leerroutes/LerenProgrammeren/module4/lootjes.py
@@ -0,0 +1,26 @@
+
+import random
+namen = [] #lege list voor namen
+lootjes = {}
+while True: #loop tot dat de gebruiker klaar is met namen
+    naam = input("Wat zijn de namen van de spelers?  ").lower()
+    if naam in namen:
+        print("dit heeft u al gekozen")
+    else:
+        namen.append(naam) #namen toevoegen aan list
+    if len(namen) >= 3:
+        vraag = input("wilt u nog een naam invoeren ? ") #vragen of gebruiker meer namen inwilt vullen
+        if vraag == "nee":
+            break #ga uit de loop als de gebruiker klaar is met namen
+shuffling = True
+while shuffling:
+    namen_shuffle = random.sample(namen, len(namen)) #gebruik maken van random.sample om namen te shuffelen
+    shuffling = False
+    for i in range(len(namen)):
+        if namen_shuffle[i] == namen[i]:
+            shuffling = True
+
+for i in range(len(namen)):
+    lootjes.update({namen[i]: namen_shuffle[i]})
+
+print(lootjes) #print die namen maar !!

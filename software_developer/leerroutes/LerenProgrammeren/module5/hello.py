e25bbc5f71e14896d4332f8eea2533cff7fd49b4
dinho <99069865@mydavinci.nl>
2023-04-13 14:04:16 +0200

    'update'

--- software_developer/leerroutes/LerenProgrammeren/module5/hello.py
+++ software_developer/leerroutes/LerenProgrammeren/module5/hello.py
@@ -1,7 +1,14 @@
-def hello():
-    print("hello from funtion town")
+cijfer = int(input('type een cijfer in'))
+def hello(cijfer):
+    i = 1
+    while(i < cijfer):
+        print(f"hello from funtion town{i}!")
+        i +=1
+        if i > cijfer:
+            break
+            
 
 
 
 
-hello()
+hello(cijfer)
---
+++ software_developer/leerroutes/LerenProgrammeren/module5/multiplier-x.py
@@ -0,0 +1,9 @@
+getal = int(input('van welk getal wil je de tafel zien?'))
+
+def tafel(getal):
+    for x in range(1,11):
+        som = getal * x
+        print(f'{x} x {getal} = {som}')
+
+
+tafel(getal)

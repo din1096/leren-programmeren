b6bb08f2792b7a32078aecda69dfdebf9a60e4fc
dinho <99069865@mydavinci.nl>
2023-04-20 10:12:25 +0200

    'update'

--- software_developer/leerroutes/LerenProgrammeren/module4/pseudocode.py
+++ software_developer/leerroutes/LerenProgrammeren/module4/pseudocode.py
@@ -1,2 +1,5 @@
-# programma vraagt namen en maakt van die namen lootjes
-# namen =
+# programma vraagt namen en maakt van die namen lootjes die worden gekkopeld aan de namen
+# namen = een lijst om de namen op te slaan
+
+# naam = ('wat zijn de namen van de spelers')
+# 
